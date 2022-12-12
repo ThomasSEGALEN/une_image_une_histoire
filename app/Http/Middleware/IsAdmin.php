d5fb61e95c01cb0d0aeadd7160b12a35de0cba6e
ThomasSEGALEN <73218293+ThomasSEGALEN@users.noreply.github.com>
2022-12-12 11:25:47 +0100

    feat/updated to check with role id

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -16,7 +16,7 @@ class IsAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!auth()->check() || !auth()->user()->is_admin) abort(403);
+        if (!auth()->check() || !auth()->user()->role_id === 1) abort(403);
 
         return $next($request);
     }
