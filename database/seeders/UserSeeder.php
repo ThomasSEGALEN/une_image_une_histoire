e71b42ed499bcecc8835be4903dee9114186296e
ThomasSEGALEN <73218293+ThomasSEGALEN@users.noreply.github.com>
2022-12-12 10:58:28 +0100

    feat/seeder to save user and admin profiles for testing

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'admin',
+                'email' => 'admin@uiuh.com',
+                'password' => Hash::make('admin'),
+                'is_admin' => 1,
+            ],
+            [
+                'name' => 'user',
+                'email' => 'user@uiuh.com',
+                'password' => Hash::make('user'),
+                'is_admin' => 0,
+            ],
+        ]);
+    }
+}
