27792a2afedb6e055913a671588e7ed07dc433d2
ThomasSEGALEN <73218293+ThomasSEGALEN@users.noreply.github.com>
2022-12-14 15:33:11 +0100

    feat/update auth views

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -5,27 +5,25 @@
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
             </a>
         </x-slot>
-
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        <h1 class="flex justify-center text-xl font-bold text-dark">Mot de passe oublié</h1>
+        <div class="mt-6 mb-4 text-sm text-gray-600">
+            {{ __("Vous avez oublié votre mot de passe ? Aucun problème. Indiquez-nous simplement votre adresse
+            mail et nous vous enverrons un lien de réinitialisation du mot de passe qui vous permettra d'en
+            choisir un nouveau.") }}
         </div>
-
-        <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
         <form method="POST" action="{{ route('password.email') }}">
             @csrf
-
-            <!-- Email Address -->
             <div>
-                <x-input-label for="email" :value="__('Email')" />
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-input-label for="email" :value="__('Adresse mail')" />
+                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                    required autofocus />
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
-
             <div class="flex items-center justify-end mt-4">
                 <x-primary-button>
-                    {{ __('Email Password Reset Link') }}
+                    {{ __('Envoyer') }}
                 </x-primary-button>
             </div>
         </form>
