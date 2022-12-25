c6326d18861136983a0459b278e5f96ead9e1157
ThomasSEGALEN <thomas.segalen.ms@hotmail.fr>
2022-12-26 00:49:28 +0100

    fix route issues

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
     return view('home');
 })->name('home');
 Route::get('/services', function () {
