f7cbd0098b0ae6a0b94a9aa4c108b45fad3afbfe
ThomasSEGALEN <73218293+ThomasSEGALEN@users.noreply.github.com>
2023-02-13 10:41:34 +0100

    Fix middleware logic

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class IsAdmin
 {
@@ -16,7 +17,7 @@ class IsAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!auth()->check() || !auth()->user()->role_id === \App\Models\Role::ADMIN) abort(403);
+        if (!Auth::check() || Auth::user()->role_id !== \App\Models\Role::ADMIN) abort(403);
 
         return $next($request);
     }
