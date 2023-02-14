e072d7a56c69cf973009c78292599945ee46213d
ThomasSEGALEN <73218293+ThomasSEGALEN@users.noreply.github.com>
2023-02-14 15:51:12 +0100

    Add pictures_manage permission

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,5 +31,9 @@ class AuthServiceProvider extends ServiceProvider
         Gate::define('users_manage', function (User $user) {
             return $user->role_id === \App\Models\Role::ADMIN;
         });
+
+        Gate::define('pictures_manage', function (User $user) {
+            return ($user->role_id === \App\Models\Role::ADMIN);
+        });
     }
 }
